5d3fa2db4e5923cfad5434a941a381b7f80ab5b6
sneha sur <snehasurece@gmail.com>
2024-09-21 13:39:19 +0530

    first commit

---
+++ redux-toolkit/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+      </header>
+    </div>
+  )
+}
+
+export default App
